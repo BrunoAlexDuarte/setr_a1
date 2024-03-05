ea1da394f381feaaded7e3f4f2d294b91d2e8af8
briato <brunoduarte9798@gmail.com>
2024-03-05 17:57:50 +0000

    Added documentation in the my_dll.h

--- MyDllLib/my_dll.h
+++ MyDllLib/my_dll.h
@@ -1,10 +1,101 @@
+/** \file my_dll.h
+ * \brief Function description Header file of the library that contains the dLL functionality
+ *
+ * \author Bruno Duarte
+ * \author Diogo Lapa
+ *
+ * \date 05-03-2024
+ * \bug No bugs to report for now
+ *
+ * */
+
 
 #ifndef __MY_DLL_H__
 #define __MY_DLL_H__
 
+/**
+ * \brief Initiates the dLL
+ *
+ * \author Bruno Duarte
+ * \author Diogo Lapa
+ *
+ * \date 05/03/2024
+ *
+ * \param[in]
+ * \param[out]
+ *
+ */
+void MyDLLInit();
 
+/**
+ * \brief Inserts the given element in the dLL and returns the id
+ *
+ * \author Bruno Duarte
+ * \author Diogo Lapa
+ *
+ * \date 05/03/2024
+ *
+ * \param[in]
+ * \param[out]
+ *
+ */
+uint_16t MyDLLInsert(unsigned char *element);
 
+/**
+ * \brief Function description
+ *
+ * \author Bruno Duarte
+ * \author Diogo Lapa
+ *
+ * \date 05/03/2024
+ *
+ * \param[in]
+ * \param[out]
+ *
+ */
+unsigned char *MyDLLRemove(uint_16t id);
 
+/**
+ * \brief Function description
+ *
+ * \author Bruno Duarte
+ * \author Diogo Lapa
+ *
+ * \date 05/03/2024
+ *
+ * \param[in]
+ * \param[out]
+ *
+ */
+unsigned char *MyDLLFind(uint_16t id);
+
+/**
+ * \brief Function description
+ *
+ * \author Bruno Duarte
+ * \author Diogo Lapa
+ *
+ * \date 05/03/2024
+ *
+ * \param[in]
+ * \param[out]
+ *
+ */
+unsigned char *MyDLLFindNext();
+
+/**
+ * \brief Function description
+ *
+ * \author Bruno Duarte
+ * \author Diogo Lapa
+ *
+ * \date 05/03/2024
+ *
+ * \param[in]
+ * \param[out]
+ *
+ */
+unsigned char *MyDLLFindPrevious();
 
 
 
