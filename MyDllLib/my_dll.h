b4538867e3b45baa4967b1518abe4b63feebbe1f
diogo <diogo.lapa@ua.pt>
2024-03-09 22:13:14 +0000

    doxygen comments

--- MyDllLib/my_dll.h
+++ MyDllLib/my_dll.h
@@ -1,13 +1,18 @@
-/** \file my_dll.h
- * \brief Function description Header file of the library that contains the dLL functionality
+/** 
+ * @file my_dll.h
+ * @brief Doubly linked list declarations.
  *
- * \author Bruno Duarte
- * \author Diogo Lapa
+ * This file contains declarations for a doubly linked list and related operations.
+ * It includes structures, function prototypes, and descriptions of their functionalities.
  *
- * \date 05-03-2024
- * \bug No bugs to report for now
+ * @author Bruno Duarte
+ * @author Diogo Lapa
  *
- * */
+ * @date 05-03-2024
+ * @bug No bugs to report for now
+ *
+ */
+
 #ifndef __MY_DLL_H__
 #define __MY_DLL_H__
 
@@ -19,6 +24,27 @@
 #include <string.h>
 #include <stdio.h>
 
+/** @struct node
+ *
+ *  @brief Represents a node in a doubly linked list.
+ *
+ *  This structure contains information for a node in a doubly linked list,
+ *  including a unique identifier ('id'), an element with a maximum size ('element'),
+ *  and indexes to the previous ('prev') and next ('next') nodes in the all_nodes array.
+ *
+ *  @var node::id
+ *  Unique identifier for the node.
+ *
+ *  @var node::element
+ *  Data element stored in the node.
+ *
+ *  @var node::prev
+ *  Index of the previous node in the all_nodes array
+ *
+ *  @var node::next
+ *  Index of the next node in the all_nodes array
+ *
+ */
 typedef struct stru_node {
 	uint16_t id;
 	unsigned char element[MAX_SIZE_ELEMENT+1];
@@ -26,6 +52,28 @@ typedef struct stru_node {
 	uint16_t next;
 } node;
 
+/** @struct dLL
+ *
+ *  @brief Represents a doubly linked list along with metadata.
+ * 
+ *  This structure contains metadata about a doubly linked list, including the max size
+ *  of each element ('size_elem'), the max size of the list ('size_list'), an array
+ *  of nodes that form the doubly linked list ('all_nodes'), and the index of the head
+ *  node ('place_middle')
+ *
+ *  @var dLL::size_elem 
+ *  Max size of each element in the list.
+ *
+ *  @var dLL::size_list 
+ *  Max size of the linked list.
+ *
+ *  @var dLL::all_nodes
+ *  Array of nodes forming the doubly linked list.
+ *
+ *  @var dLL::place_middle
+ *  Index of the head node.
+ *
+ */
 typedef struct {
 	uint16_t size_elem;
 	uint16_t size_list;
@@ -35,90 +83,103 @@ typedef struct {
 } dLL;
 
 
+
 /**
- * \brief Initiates the dLL
+ * @brief Initializes a doubly linked list with specified max element size and max list size.
  *
- * \author Bruno Duarte
- * \author Diogo Lapa
+ * This function initializes a doubly linked list by setting the element size, list size and
+ * and preparing the necessary memory structures.
+ * 
+ * @param[in] elem_size The max size of each element in the doubly linked list.
+ * @param[in] list_size The max total size or capacity of the doubly linked list.
  *
- * \date 05/03/2024
- *
- * \param[in]
- * \param[out]
+ * @note This function should be called before using any other operations on the doubly linked list.
+ * @warning Ensure that elem_size and list_size are valid and within acceptable limits.
  *
  */
 void MyDLLInit(uint16_t elem_size, uint16_t list_size);
 
 /**
- * \brief Inserts the given element in the dLL and returns the id
+ * \brief Inserts a new node with the specified ID and element into the doubly linked list.
+ * 
+ * This function inserts a new node with the given ID and element into the doubly linked list.
  *
- * \author Bruno Duarte
- * \author Diogo Lapa
+ * @param[in] id The unique identifier for the new node.
+ * @param[in] element String value to be stored in the new node.
  *
- * \date 05/03/2024
+ * @return 0 if the insertion is successful, -1 if the insertion fails (e.g., due to duplicate ID or no more space).
  *
- * \param[in]
- * \param[out]
- *
+ * @note The function returns 0 on successful insertion, and -1 if the insertion fails.
+ * @warning Ensure that the provided ID is unique within the doubly linked list.
+ * 
  */
 uint16_t MyDLLInsert(uint16_t id, unsigned char *element);
 
 /**
- * \brief Function description
+ * @brief Removes a node with the specified ID from the doubly linked list.
+ * 
+ * This function removes a node with the given ID from the doubly linked list and returns 
+ * the data element (string value) of the removed node.
  *
- * \author Bruno Duarte
- * \author Diogo Lapa
- *
- * \date 05/03/2024
- *
- * \param[in]
- * \param[out]
+ * @param[in] id The unique identifier of the node to be removed.
+ * 
+ * @return Pointer to the data element of the removed node, or NULL if the node is not found.
+ * 
+ * @note The returned pointer is the data element of the removed node.
+ * @warning Ensure that the provided ID corresponds to an existing node in the doubly linked list.  
  *
  */
 unsigned char *MyDLLRemove(uint16_t id);
 
 /**
- * \brief Function description
+ * @brief Finds the data element of a node with the specified ID in the doubly linked list.
  *
- * \author Bruno Duarte
- * \author Diogo Lapa
+ * This function searches for a node with the given ID in the doubly linked list and returns
+ * the data element (string value) of the found node.
  *
- * \date 05/03/2024
+ * @param[in] id The unique identifier of the node to be found.
+ * 
+ * @return Pointer to the data element of the found node, or NULL if the node is not found.
  *
- * \param[in]
- * \param[out]
+ * @note The returned pointer points to the data element of the found node.
+ * @warning Ensure that the provided ID corresponds to an existing node in the doubly linked list.
  *
  */
 unsigned char *MyDLLFind(uint16_t id);
 
 /**
- * \brief Function description
+ * @brief Finds the data element of the next node after the specified ID in the doubly linked list.
  *
- * \author Bruno Duarte
- * \author Diogo Lapa
+ * This function searches for the node with the given ID in the doubly linked list and returns
+ * the data element (string value) of the node that comes after it.
  *
- * \date 05/03/2024
+ * @param[in] id The unique identifier of the current node.
  *
- * \param[in]
- * \param[out]
+ * @return Pointer to the data element of the next node, or NULL if the next node is not found.
+ *
+ * @note The returned pointer points to the data element of the node that comes after the given ID.
+ * @warning Ensure that the provided ID corresponds to an existing node in the doubly linked list.
  *
  */
 unsigned char *MyDLLFindNext(uint16_t id);
 
 /**
- * \brief Function description
+ * @brief Finds the data element of the previous node before the specified ID in the doubly linked list.
  *
- * \author Bruno Duarte
- * \author Diogo Lapa
+ * This function searches for the node with the given ID in the doubly linked list and returns
+ * the data element (string value) of the node that comes before it.
  *
- * \date 05/03/2024
+ * @param[in] id The unique identifier of the current node.
  *
- * \param[in]
- * \param[out]
+ * @return Pointer to the data element of the previous node, or NULL if the previous node is not found.
+ *
+ * @note The returned pointer points to the data element of the node that comes before the given ID.
+ * @warning Ensure that the provided ID corresponds to an existing node in the doubly linked list.
  *
  */
 unsigned char *MyDLLFindPrevious(uint16_t id);
 
+// to be removed (acho eu)
 void PrintNode(uint16_t i);
 void PrintCABECA();
 
